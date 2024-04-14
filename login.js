fca109ec4fd4b9ab7e4d86b0e5309d8463b975c1
Omojola Zion <omojolazion@gmail.com>
2024-04-14 18:34:28 +0100

    Added links to link the files on the page, also edited and removed few lines

--- login.js
+++ login.js
@@ -1,26 +1,17 @@
-const emailInput = document.getElementById("email").ariaRequired;
-const passwordInput = document.getElementById("password").ariaRequired;
-const loginBtn = document.getElementById("loginBtn");
+document.addEventListener("DOMContentLoaded", function () {
+  const emailInput = document.getElementById("email");
+  const passwordInput = document.getElementById("password");
+  const loginBtn = document.getElementById("loginBtn");
 
-emailInput.addEventListener("input", toggleButtonOpacity);
-passwordInput.addEventListener("input", toggleButtonOpacity);
+  emailInput.addEventListener("input", toggleButtonOpacity);
+  passwordInput.addEventListener("input", toggleButtonOpacity);
 
-function toggleButtonOpacity() {
-  // Toggle Button opacity
-  if (emailInput.value.trim() !== "" && passwordInput.value.trim() !== "") {
-    loginBtn.style.opacity = 1;
-  } else {
-    loginBtn.style.opacity = 0.4;
+  function toggleButtonOpacity() {
+    // Toggle Button opacity
+    if (emailInput.value.trim() !== "" && passwordInput.value.trim() !== "") {
+      loginBtn.style.opacity = 1;
+    } else {
+      loginBtn.style.opacity = 0.4;
+    }
   }
-}
-
-loginBtn.addEventListener("click", function (event) {
-  event.preventDefault();
-
-  // Log input values to the console
-  console.log("Email: ", emailInput.value);
-  console.log("Password: ", passwordInput.value);
-
-  emailInput.value = "";
-  passwordInput.value = "";
 });
